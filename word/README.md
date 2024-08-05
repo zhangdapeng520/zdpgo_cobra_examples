d60a5f35e3e298d56bb0c6f99c2a8c085e6be90a
zhangdapeng520 <1156956636@qq.com>
2024-08-05 10:08:11 +0800

    新增：word增加驼峰和下划线处理

--- word/README.md
+++ word/README.md
@@ -1,3 +1,27 @@
 # word
 
 单词格式转换
+
+
+## 打包
+```bash
+go build -o word.exe
+```
+
+
+## 使用
+```bash
+.\word.exe word -h
+
+# 转大写
+.\word.exe word -u -s hello
+
+# 转小写
+.\word.exe word -u -s HELLO
+
+# 驼峰转下划线
+.\word.exe word -d -s HelloWorld
+
+# 下划线转驼峰
+.\word.exe word -c -s hello_world
+```
--- word/cmd/word.go
+++ word/cmd/word.go
@@ -4,16 +4,23 @@ import (
 	"fmt"
 	"github.com/zhangdapeng520/zdpgo_cobra"
 	"strings"
+	"unicode"
 )
 
 var (
-	str     string // 传入的字符串
-	isUpper bool   // 转换为大小
+	str               string // 传入的字符串
+	isUpper           bool   // 转换为大写
+	isLower           bool   // 转换为小写
+	isUnderline2Camel bool   // 下划线转驼峰
+	isCamel2Underline bool   // 驼峰转下划线
 )
 
 func init() {
 	wordCmd.Flags().StringVarP(&str, "str", "s", "", "请输入单词内容")
 	wordCmd.Flags().BoolVarP(&isUpper, "upper", "u", false, "是否转换为大写")
+	wordCmd.Flags().BoolVarP(&isLower, "lower", "l", false, "是否转换为小写")
+	wordCmd.Flags().BoolVarP(&isUnderline2Camel, "u2c", "c", false, "是否下划线转驼峰")
+	wordCmd.Flags().BoolVarP(&isCamel2Underline, "c2u", "d", false, "是否驼峰转下划线")
 }
 
 var wordCmd = &zdpgo_cobra.Command{
@@ -24,6 +31,36 @@ var wordCmd = &zdpgo_cobra.Command{
 		// 这个是运行子命令会直接触发的函数
 		if isUpper {
 			fmt.Println(strings.ToUpper(str))
+		} else if isLower {
+			fmt.Println(strings.ToLower(str))
+		} else if isUnderline2Camel {
+			fmt.Println(underline2Camel(str))
+		} else if isCamel2Underline {
+			fmt.Println(camel2Underline(str))
 		}
 	},
+}
+
+// 驼峰转下划线
+func camel2Underline(s string) any {
+	var output []rune
+	for i, r := range s {
+		if i == 0 {
+			// 首字母
+			output = append(output, unicode.ToLower(r))
+			continue
+		}
+		if unicode.IsUpper(r) {
+			output = append(output, '_') // 如果是大写，提前写入一个下划线
+		}
+		output = append(output, unicode.ToLower(r))
+	}
+	return string(output)
+}
+
+// 下划线转驼峰
+func underline2Camel(s string) any {
+	s = strings.Replace(s, "_", " ", -1)   // 替换下划线为空格
+	s = strings.Title(s)                   // 每个单词首字母大写
+	return strings.Replace(s, " ", "", -1) // 移除空格
 }
